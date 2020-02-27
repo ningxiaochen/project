3f40d0d185eb421887db2d007adefc9cacaa760f
ning <1012907673@qq.com>
2020-02-27 19:21:30 +0800

    '增加基础模板'

--- app.py
+++ app.py
@@ -66,6 +66,14 @@ def forge():
 
 @app.route('/')
 def index():
-    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html',user=user,movies=movies)
+    return render_template('index.html',movies=movies)
+
+@app.errorhandler(404) # 传入要处理的错误代码
+def page_not_found(e):
+    return render_template('404.html',),404
+
+@app.context_processor # 模板上下文处理函数
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
